fd645e1c68ca8815e71e8837faacef413e88b183
AnthonyYates <Tony@anthonyyates.com>
2019-09-04 13:50:11 +0200

    Initial commit with code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SuperOffice.DevNet.Online.SystemUser.NetCore.Console
-.NET Core (2.2) console application that demonstrates how to exchange system user token for a system user ticket.
+
+Application that demonstrates how to use .NET Core (2.2) and exchange a SuperOffice Online system user token for a system user ticket.
+
+## Getting Started From Scratch
+
+Create a .net core console application. Right-click project and in the Add menu, click Connected Service.
+
+![AddService](Assets/images/AddConnectedService.png)
+
+In the Connected Services pane, click the __Microsoft WCF Web Services Reference Provider__ option.
+
+![Connected Services](Assets/images/SelectWCFServiceReference.png)
+
+In the Configure WCF Web Service Reference pain, click __Browse__, then locate and select the __PartnerSystemUserService.wsdl__ file.
+
+![WSDL Select](Assets/images/BrowsePartnerSystemUserServiceWSDL.png)
+
+When the service definition has finished loading, expand the available Services and click the __IPartnerSystemUserService__ interface, then click __Finish__.
+
+![Generate Code](Assets/images/SelectInterfaceThenFinish.png)
+
+Observe the generated code based on the WSDL definition.
+
+![Generate Code](Assets/images/SoSystemUserServiceCode.png)
+
+Review the code in the project to see how to call this SOAP service and exchange a system user token for a system user ticket.
---
+++ Source/Program.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SoSystemUserService;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SuperOffice.DevNet.Online.SystemUser.NetCore.ConsoleApp
+{
+    public static class Constants
+    {
+
+        // set the environment system user endpoint
+        public const string SystemUserEndpoint = "https://sod.superoffice.com/Login/services/PartnerSystemUserService.svc";
+        public const string SuperIdCertificateThumbprint = "";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var success = false;
+
+            // App Secret / Token
+
+            var appTk = "YOUR_APPLICATION_TOKEN";
+
+            // SystemToken - this is obtained from the JWT returned via user interaction sign-in
+
+            var sysTk = "YOUR APPLICATION NAME-SomeRandomCode";
+
+            // Customer ContextIdentifier
+
+            var appcx = "Cust12345";
+
+            // read the private certificate file to sign the request to exchange token for ticket
+
+            var certificate = File.ReadAllText(AppContext.BaseDirectory + "ServiceConsoleCert.xml");
+
+            // sign the token
+
+            var signedToken = Sign(certificate, sysTk);
+
+            // exchange the token for a system user ticket
+
+            var response = Authenticate(appTk, appcx, signedToken);
+
+            if (response.IsSuccessful)
+            {
+                var superOfficeCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(
+                       AppContext.BaseDirectory + "SuperOfficeFederatedLogin.crt");
+
+                // validate JWT token from SuperOffice
+
+                var validToken = ValidateAndDecode(response.Token, new[] { new X509SecurityKey(superOfficeCertificate) });
+
+                // validate response...
+                // extract claims from the token
+                var claims = validToken.Claims.Select(c => new Claim(c.Type, c.Value, ClaimValueTypes.String)).ToArray();
+
+                // get the system user ticket (which is used in Http request header "SOTicket ${sysUserTicketValue}"
+
+                var systemUserTicket = claims.FirstOrDefault(c => c.Type.Contains("ticket")).Value;
+
+                // Online base REST endpoint url
+
+                var webapi_url = claims.FirstOrDefault(c => c.Type.Contains("webapi")).Value;
+
+                /* User systemUserTicket to create REST calls to the webapi_url...
+                    GET /Cust26759/api/v1/MDOList/category?flat=True HTTP/1.1
+                    Host: sod.superoffice.com
+                    accept: application/json
+                    authorization: SOTICKET 7T:MAA3AGYAMQBlAGQAZAAxAGQAZQAwADgAYgAxAGEAYwBkADYAOAA0ADcAMQA2ADkAOQBhADQAZgBiADMAOQAyADsAMgAwADgANwAzADEANQAwADQAMwA7AEMAdQBzAHQAMgA2ADcANQA5AA==
+                    SO-AppToken: f2398a3a7wer3759d4b220e9a9c94321
+                    accept-language: en
+                */
+                success = true;
+
+
+
+                return;
+            }
+
+            Console.WriteLine(success ? "Success!" : "Failed!");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadLine();
+        }
+
+        private static string Sign(string privateKey, string token)
+        {
+            string str;
+            string utcNow = DateTime.UtcNow.ToString("yyyyMMddHHmm");
+            string signThis = string.Concat(token, ".", utcNow);
+            using (RSACryptoServiceProvider rsaCryptoProvider = new RSACryptoServiceProvider())
+            {
+                rsaCryptoProvider.FromXmlString2(privateKey);
+                byte[] signature = rsaCryptoProvider.SignData(Encoding.UTF8.GetBytes(signThis), "SHA256");
+                str = string.Concat(signThis, ".", Convert.ToBase64String(signature));
+            }
+            return str;
+        }
+
+        public static AuthenticationResponse Authenticate(string appToken, string contextId, string signedToken)
+        {
+            AuthenticationRequest ar = new AuthenticationRequest(appToken, contextId, signedToken, TokenType.Jwt);
+
+            var client = new PartnerSystemUserServiceClient(
+                PartnerSystemUserServiceClient.EndpointConfiguration.BasicHttpBinding_IPartnerSystemUserService1);
+
+            var result = client.AuthenticateAsync(ar);
+            result.Wait();
+
+            return result.Result;
+        }
+
+        private static JwtSecurityToken ValidateAndDecode(string jwt, IEnumerable<SecurityKey> signingKeys)
+        {
+            var validationParameters = new TokenValidationParameters
+            {
+                // Clock skew compensates for server time drift.
+                // We recommend 5 minutes or less:
+                ClockSkew = TimeSpan.FromMinutes(5),
+                // Specify the key used to sign the token:
+                IssuerSigningKeys = signingKeys,
+                RequireSignedTokens = true,
+                // Ensure the token hasn't expired:
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                // Ensure the token audience matches our audience value (default true):
+                ValidateAudience = false,
+                ValidAudience = "spn://db_serial",
+                // Ensure the token was issued by a trusted authorization server (default true):
+                ValidateIssuer = true,
+                ValidIssuer = "SuperOffice AS"
+            };
+
+            try
+            {
+                var claimsPrincipal = new JwtSecurityTokenHandler()
+                    .ValidateToken(jwt, validationParameters, out var rawValidatedToken);
+
+                return (JwtSecurityToken)rawValidatedToken;
+                // Or, you can return the ClaimsPrincipal
+                // (which has the JWT properties automatically mapped to .NET claims)
+            }
+            catch (SecurityTokenValidationException stvex)
+            {
+                // The token failed validation!
+                // TODO: Log it or display an error.
+                throw new Exception($"Token failed validation: {stvex.Message}");
+            }
+            catch (ArgumentException argex)
+            {
+                // The token was not well-formed or was invalid for some other reason.
+                // TODO: Log it or display an error.
+                throw new Exception($"Token was invalid: {argex.Message}");
+            }
+        }
+    }
+}
